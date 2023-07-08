875ed15a2ed81e49e83d0a2cb9b7c9c6bd2e866d
Yukrasii <4019291@gmail.com>
2023-07-08 13:32:28 +0300

    add while loops

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-/** This "while" cycle with boolean condition demonstrates how we can stop it by using "break". 
- * 
+
+let counter1 = 0;
+
+/**
+ * This fucntion accepts the number, start the loop 'while', checks if number value is even and if it is, it shows the value on console. The way to break loop here is the condition inside it.
+ * @param {number} counter1 
+ * @returns {string}
  */
 
-let counter = 0;
-
-while(true) {
-
-    console.log('iteration')
-
-    if (++counter >= 3) {
-        break;
+function loop1 (counter1) {
+    console.log(`counter = ${counter1}`)
+    
+    while (counter1 <= 10) {
+        
+        if (++counter1 %2 === 0) {
+            console.log(`counter = ${counter1}`)
     }
+    
 }
 
-/** This "while" cycle accepts variable and demonstrates how we can stop it by change the variabe value from true to false by asking user via confirm function. 
- * 
+return console.log(`after loop is stoped the value 'counter1' is ${counter1}`)
+
+}
+
+loop1(counter1)
+
+/**
+ * This fucntion accepts the number(counter), start the loop 'while', checks if number value is even and if it is, it shows the value on console.
+ *  The way to break loop here is the 'break' operator when the value of counter exceeds condition.
+ * @param {number} counter2 
+ * @returns {string}
  */
 
-let shouldConfirm = true
+let counter2 = 0;
 
+function loop2 (counter2) {
+while (true) {
+    
+    if (counter2 %2 === 0) {
+        console.log(`counter = ${counter2}`)
+    }
 
-while(shouldConfirm) {
-    console.log('Iteration')
-shouldConfirm = confirm('Continue cycle?')
+    if (counter2++ > 10) {
+        break
+    } 
 }
+    
+    return console.log(`after loop is stoped the value 'counter2' is ${counter2}`)
+
+}
+
+
+loop2(counter2)
