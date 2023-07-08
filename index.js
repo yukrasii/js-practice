ecf3a39d116f9a19120baf653c501dbed0bafd47
Yukrasii <4019291@gmail.com>
2023-07-08 20:44:26 +0300

    add loop 'while'. Case password

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
+let counter = 0
+
 let correctPassword = 'qwe';
 
 let userPassword = prompt('Enter password');
 
 /**
- * Function accepts parameters of two values (passwords) and compares them. If they are identical, function returns *success*, 
- * if not, the loop 'for' inside gives 5 attempts to retry and if they are exhausted function returns alert about this.
+ * The case with authentication. Loop fot accepts parameters of two values (passwords) and compares them. If they are identical, function returns *success*, 
+ * if not, the loop 'while' inside gives 5 attempts to retry and if they are exhausted loop returns alert about this.
  * 
  * @param {string} correctPassword 
  * @param {string} userPassword 
  * @returns {prompt |alert}
  */
 
-function authentication (correctPassword, userPassword) {
-    
-    if (userPassword === correctPassword) {
-        alert('password correct')
+while (counter <= 5) {
+    if (userPassword !== correctPassword) {
+        counter++
+        userPassword = prompt(`Incorrect password. Try again: ${counter}`)
+        }
         
-    } else {
-        for (let i = 0; i < 5; i++) {
-            userPassword = prompt('Wrong password. Try again')
-            if (userPassword === correctPassword) {
-                alert('Password correct')
-                return;
-            }
-        } 
-    }
-    alert ('Too many attempts. Access denied')
-    }
+    else if (userPassword === correctPassword) {
+        alert ('Password Correct')
+        break}
+        
+        if (counter === 5) {
+            alert ('Too many attempts. Try again later')
+            break
 
-    authentication (correctPassword, userPassword)
+        }
+        
+    }
 
 
