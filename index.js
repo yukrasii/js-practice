469eb58a007841d4ea664401b643fec0ba045d19
Yukrasii <4019291@gmail.com>
2023-07-08 20:42:17 +0300

    add 'for' loop. Case password

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+let correctPassword = 'qwe';
 
-let counter = 0;
+let userPassword = prompt('Enter password');
 
 /**
- * This fucntion accepts the number, start the loop 'do while' just to show that one itteration will occur under any conditions.
- *      
- * @param {number} counter 
- * @returns {string}
+ * Function accepts parameters of two values (passwords) and compares them. If they are identical, function returns *success*, 
+ * if not, the loop 'for' inside gives 5 attempts to retry and if they are exhausted function returns alert about this.
+ * 
+ * @param {string} correctPassword 
+ * @param {string} userPassword 
+ * @returns {prompt |alert}
  */
 
-function loop (counter) {
+function authentication (correctPassword, userPassword) {
     
-        do {
-        counter++
-        console.log(counter)
+    if (userPassword === correctPassword) {
+        alert('password correct')
+        
+    } else {
+        for (let i = 0; i < 5; i++) {
+            userPassword = prompt('Wrong password. Try again')
+            if (userPassword === correctPassword) {
+                alert('Password correct')
+                return;
+            }
+        } 
+    }
+    alert ('Too many attempts. Access denied')
+    }
 
-    } while (false)
-    
+    authentication (correctPassword, userPassword)
 
-return console.log(`after loop is stoped the value 'counter' is ${counter}`)
 
-}
-
-loop(counter)
