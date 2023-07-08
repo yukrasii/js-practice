ce44664a2c7132a2f0c569a4ff509d913853d074
Yukrasii <4019291@gmail.com>
2023-07-08 16:53:32 +0300

    add {do} (while) loop

--- index.js
+++ index.js
@@ -2,50 +2,23 @@
 let counter = 0;
 
 /**
- * This fucntion accepts the number, start the loop 'while', checks if number value is even and if it is, it shows the value on console.
- *  The way to break loop here is the condition inside it.
+ * This fucntion accepts the number, start the loop 'do while' just to show that one itteration will occur under any conditions.
  *      
  * @param {number} counter 
  * @returns {string}
  */
 
-function loop1 (counter) {
+function loop (counter) {
     
-        while (counter <= 10) {
-        //The function has been rewritten(shortened by using addition assignment operator):
+        do {
+        counter++
         console.log(counter)
-        counter+=2
-    }
+
+    } while (false)
     
 
 return console.log(`after loop is stoped the value 'counter' is ${counter}`)
 
 }
 
-loop1(counter)
-
-/**
- * This fucntion accepts the number(counter), start the loop 'while', checks if number value is even and if it is, it shows the value on console.
- *  The way to break loop here is the 'break' operator when the value of counter exceeds condition.
- *  
- * @param {number} counter 
- * @returns {string}
- */
-
-function loop2 (counter) {
-while (true) {
-    //The function has been rewritten(shortened by using addition assignment operator):
-    console.log(counter)
-    counter+=2
-
-    if (counter > 10) {
-        break
-    } 
-}
-    
-    return console.log(`after loop is stoped the value 'counter' is ${counter}`)
-
-}
-
-
-loop2(counter)
+loop(counter)
