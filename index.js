63df5def05d9c48f53ff7876a34d8e864e682e02
Yukrasii <4019291@gmail.com>
2023-07-10 10:32:24 +0300

    add loops which count numbers

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
-let counter = 0
+// /**
+//  * This loop 'for' consoles numbers from 30 to 0
+//  */
 
-let correctPassword = 'qwe';
+for (let i = 30; i >= 0; i--) {
+    console.log (i)
+}
 
-let userPassword = prompt('Enter password');
+
+
+
+// /**
+//  * This loop 'while' consoles numbers from 30 to 0
+//  */
+
+let i = 30;
+
+while(i >= 0) {
+    console.log(i)
+    i--;
+}
+
+
+
+// /**
+//  * This loop 'while' consoles numbers from 30 to 0
+//  */
+
+i = 100;
+
+do {
+    console.log(i)
+    i--;
+} while (i >= 0)
+
 
 /**
- * The case with authentication. Loop fot accepts parameters of two values (passwords) and compares them. If they are identical, function returns *success*, 
- * if not, the loop 'while' inside gives 5 attempts to retry and if they are exhausted loop returns alert about this.
- * 
- * @param {string} correctPassword 
- * @param {string} userPassword 
- * @returns {prompt |alert}
+ * This loop 'for' counts summary numbers from 1 to 100 by adding the iteration value to sum variable. 
  */
 
-while (counter <= 5) {
-    if (userPassword !== correctPassword) {
-        counter++
-        userPassword = prompt(`Incorrect password. Try again: ${counter}`)
-        }
-        
-    else if (userPassword === correctPassword) {
-        alert ('Password Correct')
-        break}
-        
-        if (counter === 5) {
-            alert ('Too many attempts. Try again later')
-            break
+let sum = 0
 
-        }
-        
-    }
+for (let i = 1; i <= 100; i++) {
+    
+    console.log(sum += i)
+}
 
+/**
+ * This loop 'while' counts summary numbers from 1 to 100 by adding the iteration value to sum variable. 
+ */
 
+let counter = 1;
+sum = 0
+
+while(counter <= 100) {
+    console.log(sum += counter)
+    counter++
+}
