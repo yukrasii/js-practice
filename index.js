1900cea9f91ac49bb06b991e0c4fe99539c3ad65
Yukrasii <4019291@gmail.com>
2023-07-14 20:12:37 +0300

    add function greeting with template string

--- index.js
+++ index.js
@@ -14,15 +14,55 @@
 // const user1 = new User ('Petro', 'Ivanenko', 'male', 32)
 
 
-const user2 = {
+const user1 = {
     firstName: 'Test',
     lastName: 'Testenko',
     age: 18,
     isMale: true
 }
 
-function sayHi (user) {
+const user2 = {
+    firstName: 'Aaa',
+    lastName: 'Aaa',
+    age: 18,
+    isMale: true
+}
+
+
+const user3 = 'sdsfs'
+
+
+/**
+ * This function accepts object of user and returns greeting by using its property.
+ *  Also function checks whether the data passed to it is an object.
+ * Func uses template string
+ * 
+ * @param {objectbject} user 
+ * @returns {string}
+ */
+function greetUser (user) {
+    if (typeof user !== 'object') {
+        return 'Hello, Guest!'
+    }
     
     return `Hello, ${user.firstName} ${user.lastName}`
 }
 
+/**
+ * This function accepts object of user and returns greeting by using its property.
+ *  Also function checks whether the data passed to it is an object (by checking the presence a property of the object )
+ * Func also uses template string
+ * 
+ * @param {objectbject} user 
+ * @returns {string}
+ */
+
+function greetUser2 (user) {
+    if (user.firstName === undefined) {
+        return 'Hello, Guest!'
+    }
+    
+    return `Hello, ${user.firstName} ${user.lastName}`
+}
+
+ 
