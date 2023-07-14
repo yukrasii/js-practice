ea42d5f867f5bcac4151c297f210d20da1cf572b
Yukrasii <4019291@gmail.com>
2023-07-14 10:25:17 +0300

    add function that interacts with object

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const User = function (firstName, lastName, sex, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.sex = sex;
-    this.age = age;
-    this.speak = function () {
-        console.log('Is speaking')        
-    }
-    this.fullName = function() {
-console.log(this.firstName + ' ' + this.lastName)
-    }
+// const User = function (firstName, lastName, sex, age) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.sex = sex;
+//     this.age = age;
+//     this.speak = function () {
+//         console.log('Is speaking')        
+//     }
+//     this.fullName = function() {
+// console.log(this.firstName + ' ' + this.lastName)
+//     }
+// }
+
+// const user1 = new User ('Petro', 'Ivanenko', 'male', 32)
+
+
+const user2 = {
+    firstName: 'Test',
+    lastName: 'Testenko',
+    age: 18,
+    isMale: true
 }
 
-const user1 = new User ('Petro', 'Ivanenko', 'male', 32)
+function sayHi (user) {
+    
+    return `Hello, ${user.firstName} ${user.lastName}`
+}
+
