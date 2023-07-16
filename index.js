38ba0d630421ca8cb0467fd66545c1ec2c216fef
Yukrasii <4019291@gmail.com>
2023-07-16 17:48:13 +0300

    add filter method example

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const array = [-10, -7, 3, 4, 9, 6, -6, -8, 5, 10, -1, -2, -3, -4, -5, 7, 2, 8, -9, 1]
 
+
 /**
- * This function sorts data (numbers) in array from high to low value
- * @param {number} currentValue 
- * @param {number} nextValue 
- * @returns {number}
+ * This func says to *filter* method how to filter the oddNumbers in array 
+ * @param {} number 
+ * @returns 
  */
-
-//also we can use just if/else
-function compareFunction(currentValue, nextValue) {
-    if (currentValue > nextValue) {
-        return -1
-    }
-
-    if (currentValue < nextValue) {
-        return 1
-    }
-
-    if (currentValue = nextValue) {
-        return 0
-    }
-} 
-
-array.sort(compareFunction)
+const isOdd = function (number) {
+    return number %2 !== 0
+}
 
 
+const sortedArray = array.filter(isOdd)
