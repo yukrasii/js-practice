10101573e8bdb027c8807df7eb687b2c0dd2b032
Yukrasii <4019291@gmail.com>
2023-07-16 17:43:08 +0300

    method sort (func who sorts numbers). change sort func add func caller

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const array = [ 1,2,3,4,5,6,7,8,9,10,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+const array = [-10, -7, 3, 4, 9, 6, -6, -8, 5, 10, -1, -2, -3, -4, -5, 7, 2, 8, -9, 1]
 
 /**
- * This function sorts data (numbers) in array from low to high value
+ * This function sorts data (numbers) in array from high to low value
  * @param {number} currentValue 
  * @param {number} nextValue 
  * @returns {number}
  */
+
+//also we can use just if/else
 function compareFunction(currentValue, nextValue) {
     if (currentValue > nextValue) {
         return -1
@@ -20,5 +22,6 @@ function compareFunction(currentValue, nextValue) {
     }
 } 
 
-//also we can use just if/else
+array.sort(compareFunction)
+
 
