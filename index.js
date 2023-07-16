1f576971856a54d8699153a32f9341eba04dbf03
Yukrasii <4019291@gmail.com>
2023-07-16 18:22:57 +0300

    add findIndex

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const array = [-10, -7, 3, 4, 9, 6, -6, -8, 5, 10, -1, -2, -3, -4, -5, 7, 2, 8, -9, 1]
 
+const index = array.findIndex(function (number) {
+    return number > 4
+})
 
-/**
- * This func says to *filter* method how to filter the oddNumbers in array 
- * @param {} number 
- * @returns 
- */
-const isOdd = function (number) {
-    return number %2 !== 0
-}
-
-
-const sortedArray = array.filter(isOdd)
+//index is 4 (9)
