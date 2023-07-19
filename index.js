48555fc32b6951ad71dc839d7ccc87f6c3caaf89
Yukrasii <4019291@gmail.com>
2023-07-19 18:34:22 +0300

    add random users func constructor + map+filter

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
-const array = [-10, -7, 3, 4, 9, 6, -6, -8, 5, 10, -1, -2, -3, -4, -5, 7, 2, 8, -9, 1]
+/**
+ * This function-constructor can make objects of users
+ */
+const User = function (firstName, lastName, email, age, isMale, isSubscribed) {
+    this.firstName = firstName,
+    this.lastName = lastName,
+    this.email = email,
+    this.age = age,
+    this.isMale = isMale,
+    this.isSubscribed = isSubscribed
+ }
 
-const index = array.findIndex(function (number) {
-    return number > 4
-})
+ /**
+  * This function makes an amount of random users
+  * @param {number} amount 
+  * @returns {Array}
+  */
+ 
+ function createUsers (amount) {
+    const usersArray = []
 
-//index is 4 (9)
+    for(let i = 0; i < amount; i++) {
+    const user = new User (
+        `John ${i}`,
+         `Doe ${i}`,
+          `email${i}@gmail.com`,
+           Math.round(Math.random() * 100),
+            Math.random() > 0.5,
+            Math.random() > 0.5
+             )
+                
+            usersArray.push(user)
+        } 
+            return usersArray
+    }
+
+    usersArray1 = createUsers(20)
+
+    console.table(usersArray1)
+
+
+    //get array of emails (map):
+
+    const usersEmails = usersArray1.map(function (user) {
+                return user.email;
+            })
+
+
+            //get array of adult users (filter)
+            const adultUsers = usersArray1.filter(function(user) {
+                return user.age > 17
+            })
